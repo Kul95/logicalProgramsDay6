300497e204bd6c169ea1da84dbc92bf3b3b3cdc5
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-20 14:19:07 +0530

    Added : Prime number program

--- src/com/bridglabz/LogicalProgramsDay6/LogicalProgramsDay6.java
+++ src/com/bridglabz/LogicalProgramsDay6/LogicalProgramsDay6.java
@@ -4,6 +4,7 @@ public class LogicalProgramsDay6 {
     public static void main(String[] args) {
         fibonacciSeries();
         perfectNumber();
+        primeNumber();
     }
     public static void fibonacciSeries(){
         System.out.println("Enter the number:");
@@ -33,6 +34,22 @@ public class LogicalProgramsDay6 {
         }
         else{
             System.out.println("Not a perfect number");
+        }
+    }
+    public static void primeNumber(){
+        System.out.println("Enter the number: ");
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int count=0;
+        for(int i=2;i<=number-1;i++){
+            if(number%i==0){
+                count=count+1;
+            }
+        }
+        if(count>0){
+            System.out.println(number+": is not a prime number.");
+        }else{
+            System.out.println(number+" : is a prime number.");
         }
     }
 }
